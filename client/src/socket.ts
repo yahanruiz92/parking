d27f8b898e3104992613ac0f5ef32d7e88ba36a3
yahanruiz92 <yahankevinr@gmail.com>
2024-11-25 07:15:21 -0600

    Update socket.ts

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,3 +1,9 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('parking-yahan-efgzdha4aadxfcby.canadacentral-01.azurewebsites.net');
+// En desarrollo
+const isDev = import.meta.env.DEV;
+const serverUrl = isDev 
+  ? 'http://localhost:3000'
+  : 'https://parking-yahan.azurewebsites.net';
+
+export const socket = io(serverUrl);
